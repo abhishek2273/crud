80856b8599206d58fd74be3c7a72d06e8c609502
Abhishek Sen <105163544+abhishek2273@users.noreply.github.com>
2023-03-10 01:04:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,14 +10,6 @@ const url = ('mongodb://127.0.0.1/crud')
 mongoose.connect(url, ()=>{
     console.log("DateBase is connected.........");
 
-    // schema.create({
-    //     username: "Abhishek",
-    //     email: "Raju1203@gmail.com",
-    //     password: 123456,
-    //     confirmPassword: 123456
-    // })
-})
-
 app.use(express.json());
 app.use('/crud',route);
 
